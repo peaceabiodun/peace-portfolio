e6971046fdf2f7c326bffc375b1d39c5edcccd96
peaceabiodun <abiodunpeace8@gmail.com>
2023-08-09 18:58:40 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -19,5 +18,5 @@ module.exports = withMT({
     },
   },
   plugins: [],
-})
+}
 
