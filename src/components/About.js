f4ed02dd3c44f2c2bcb888127fc1bfd74c8ddf1e
peaceabiodun <abiodunpeace8@gmail.com>
2023-08-09 18:30:41 +0100

    bug fixes

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <h1 className='text-lg font-bold text-center mb-4'>About Me</h1>
                 <div className='flex flex-col sm:flex-row w-full items-center sm:justify-center gap-10'>
                     <div className='text-xs'>
-                        <img src={Pic1} alt='my-pic' className='min-w-[190px] h-[300px] border-2 border-black' />
+                        <img src={Pic1} alt='my-pic' className='min-w-[180px] h-[290px] border-2 border-black' />
                         <p className='font-bold mt-3'>Peace Abiodun</p>
                         <p className=''>Software Engineer</p>
                     </div>
@@ -15,7 +15,7 @@ const About = () => {
                     <div className='max-w-[400px] '>
                         <p className='font-bold text-sm'>Bio:</p>
                         <p className='text-xs sm:text-sm'> I'm a Frontend Engineer dedicated to building high quality web applications with a focus on scalability and performance. <br />
-                            Currently, I work as a Frontend Engineer at Food Court, where I use my experience with JavaScript, TypeScript, ReactJS, Tailwind CSS, Sass, Git and APIs alongside a talented team to deliver seamless user experiences and build innovative solutions that cater to the ever-changing demands of the food industry.  <br /> I also work as an OSP fiber Engineer at Huawei, Nigeria.<br /> 
+                            Currently, I work as a Frontend Engineer at Food Court, where I use my experience with JavaScript, TypeScript, ReactJS, Tailwind CSS, Sass, Git and APIs alongside a talented team to deliver seamless user experiences and build innovative solutions that cater to the ever-changing demands of the food industry.  <br /> I also work as an OSP fiber Engineer.<br /> 
 
                             When I'm not immersed in work, I love to have a good time, travel and explore.
                         </p>
--- src/components/work.js
+++ src/components/work.js
@@ -1,40 +1,36 @@
-import { Carousel } from "@material-tailwind/react";
+import Slider from "react-slick";
 import { projects } from "../utils/data";
 import { Link } from "react-router-dom";
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Work = () => {
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+  };
     return ( 
         <div className='px-6 sm:px-10 pt-6 sm:pt-10 pb-[60px] ' id='work'>
             <h1 className='text-[16px] font-bold text-center mb-4'>Some of My Latest work </h1>
 
-            <div className="flex justify-center">
-                <Carousel 
-                  className='w-[100%] sm:w-[80%] ' 
-                  navigation={({ setActiveIndex, activeIndex, length }) =>(
-                    <div className="absolute bottom-0 left-2/4 z-30 flex -translate-x-2/4 gap-2 bg-white">
-                    {new Array(length).fill("").map((_, i) => (
-                      <span
-                        key={i}
-                        className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                          activeIndex === i ? "w-8 bg-[#787879]" : "w-4 bg-[#ceced3]"
-                        }`}
-                        onClick={() => setActiveIndex(i)}
-                      />
-                    ))}
-                  </div>
-                  )}>
-                    {projects.map((item)=>(
-                    <div key={item.id} className='flex flex-col items-center'>
-                        <Link  to={item.link}><img src={item.image} alt='' className='w-full h-full border shadow-md cursor-pointer' /></Link>
-                        <Link to={item.link} className='font-bold text-sm my-4 hover:text-[#8a2159]'>{item.title}</Link>
-                        <p className='text-sm font-medium max-w-[340px] mb-3'>{item.description}</p>
+            <div className="flex items-center justify-center">
+              <Slider {...settings} className="w-[100%] sm:w-[80%] flex">
+                {projects.map((item)=>(
+                  <div key={item.id} className='flex flex-col items-center '>
+                    <Link  to={item.link}><img src={item.image} alt='' className='w-full h-full border shadow-md cursor-pointer' /></Link>
+                    <div className="my-4 flex flex-col items-center">
+                      <Link to={item.link} className='font-bold text-sm hover:text-[#8a2159]'>{item.title}</Link>
+                      <p className='text-sm font-medium max-w-[340px] mt-2'>{item.description}</p>
                     </div>
-                    ))}
-
                     
-                </Carousel>
+                  </div>
+                    ))}
+              </Slider>
             </div>
         </div>
      );
