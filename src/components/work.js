9095c29d2b38907356931f87443d9bf8d3677680
peaceabiodun <abiodunpeace8@gmail.com>
2023-12-05 09:47:50 +0100

    collab geek

--- src/components/work.js
+++ src/components/work.js
@@ -1,39 +1,55 @@
-import Slider from "react-slick";
-import { projects } from "../utils/data";
-import { Link } from "react-router-dom";
+import Slider from 'react-slick';
+import { projects } from '../utils/data';
+import { Link } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Work = () => {
-
   const settings = {
     dots: true,
-    infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    swipe: true,
+    draggable: true,
   };
-    return ( 
-        <div className='px-6 sm:px-10 pt-6 sm:pt-10 pb-[60px] ' id='work'>
-            <h1 className='text-[16px] font-bold text-center mb-4'>Some of My Latest work </h1>
+  return (
+    <div className='px-6 sm:px-10 pt-6 sm:pt-10 pb-[60px] ' id='work'>
+      <h1 className='text-[16px] font-bold text-center mb-4'>
+        Some of My Latest Works{' '}
+      </h1>
 
-            <div className="flex items-center justify-center">
-              <Slider {...settings} className="w-[100%] sm:w-[80%] flex">
-                {projects.map((item)=>(
-                  <div key={item.id} className='flex flex-col items-center '>
-                    <Link  to={item.link}><img src={item.image} alt='' className='w-full h-full border shadow-md cursor-pointer' /></Link>
-                    <div className="my-4 flex flex-col items-center">
-                      <Link to={item.link} className='font-bold text-sm hover:text-[#8a2159]'>{item.title}</Link>
-                      <p className='text-sm font-medium max-w-[340px] mt-2'>{item.description}</p>
-                    </div>
-                    
-                  </div>
-                    ))}
-              </Slider>
+      <div className='flex items-center justify-center'>
+        <Slider autoplay {...settings} className='w-[100%] sm:w-[80%] flex'>
+          {projects.map((item) => (
+            <div key={item.id} className='flex flex-col items-center '>
+              <Link to={item.link}>
+                <img
+                  src={item.image}
+                  alt=''
+                  className='w-full h-full border shadow-md cursor-pointer'
+                />
+              </Link>
+              <div className='my-4 flex flex-col items-center'>
+                <Link
+                  to={item.link}
+                  className='font-bold text-sm hover:text-[#8a2159]'
+                >
+                  {item.title}
+                </Link>
+                <p className='text-sm font-medium max-w-[340px] mt-2'>
+                  {item.description}
+                </p>
+              </div>
             </div>
-        </div>
-     );
-}
- 
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
 export default Work;
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,29 +1,39 @@
 import Wek from '../assets/image/wek.PNG';
 import Lamore from '../assets/image/lamore.PNG';
 import Farm from '../assets/image/farm.PNG';
-
+import Collab from '../assets/image/collab.PNG';
 
 export const projects = [
-    {
-        id: 1, 
-        image: Wek, 
-        title: 'Wekbod', 
-        link: 'https://wekbod.netlify.app/',
-        description: 'This web application facilitates the connection between job seekers and their desired positions, helping them find their dream jobs.'
-    },
-    {
-        id: 2, 
-        image: Lamore, 
-        title: 'Lamore', 
-        link: '/redirect',
-        description: 'This is an event management web Platform. You can organize a wide range of events and collaborate with various vendors who provide services tailored to your various event requirements.'
-    },
-    {
-        id: 3, 
-        image: Farm, 
-        title: 'Olabisis Farm', 
-        link:'https://olabisis-farm.netlify.app/',
-        description: "I am currently developing a website for my mom's farm. From this platform, you have the convenience of purchasing fresh farm and livestock products, regardless of your location within Nigeria."
-    },
-
+  {
+    id: 1,
+    image: Collab,
+    title: 'Collab geek',
+    link: 'https://collabgeek.co/',
+    description:
+      'This a website I built for a client. It is a platform where creatives work together on various activities to create beutiful things.',
+  },
+  {
+    id: 2,
+    image: Wek,
+    title: 'Wekbod',
+    link: 'https://wekbod.netlify.app/',
+    description:
+      'This web application facilitates the connection between job seekers and their desired positions, helping them find their dream jobs.',
+  },
+  {
+    id: 3,
+    image: Lamore,
+    title: 'Lamore',
+    link: '/redirect',
+    description:
+      'This is an event management web Platform. You can organize a wide range of events and collaborate with various vendors who provide services tailored to your various event requirements.',
+  },
+  {
+    id: 4,
+    image: Farm,
+    title: 'Olabisis Farm',
+    link: 'https://olabisis-farm.netlify.app/',
+    description:
+      "I am currently developing a website for my mom's farm. From this platform, you have the convenience of purchasing fresh farm and livestock products, regardless of your location within Nigeria.",
+  },
 ];
