c69c030d43a2a4b051ee4eebc37f28a621996859
peaceabiodun <abiodunpeace8@gmail.com>
2024-05-27 17:16:46 +0100

    first commit

--- src/pages/main-page/index.js
+++ src/pages/main-page/index.js
@@ -1,11 +1,17 @@
 const MainPage = () => {
   return (
-    <div className='bg-[#111111] w-full p-5 sm:p-10'>
-      <div className='flex justify-between gap-4'>
-        <div className='py-2 px-3 text-[#f0ecec] bg-[#8d8b8b50] rounded-full'>
+    <div className='bg-main w-full h-full min-h-screen p-5 sm:p-10'>
+      <div className='flex justify-center md:justify-between gap-4'>
+        <div className='hidden md:block text-[#f0ecec] text-lg font-semibold'>
           Portfolio
         </div>
+        <div className=' flex gap-4 p-3 text-[#f0ecec] bg-[#8d8b8b50] rounded-full text-sm font-semibold '>
+          <p className='hover:text-[#8f3a7a] cursor-pointer'>Projects</p>
+          <p className='hover:text-[#8f3a7a] cursor-pointer'>Get In Touch</p>
+        </div>
       </div>
+
+      <div></div>
     </div>
   );
 };
